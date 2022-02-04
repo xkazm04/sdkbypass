5a882941ed925efefe82af7d270fb6f502646a06
Nick Papirniy <nikolaspapirniywork@gmail.com>
2022-02-04 16:54:49 +0200

    feat(cra): init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as Web3 from 'web3'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log(Web3)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Web3 loaded</h1>
+                <p>
+                    {JSON.stringify(Web3)}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
